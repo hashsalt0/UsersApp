d7309d0963418e6cb8e8e77426c4c646fe72766e
snehi <snehil101@gmail.com>
2022-03-30 13:03:18 +0530

    Fixed exit menu

--- user_app/bin/menu/sub_menus/exit_menu.dart
+++ user_app/bin/menu/sub_menus/exit_menu.dart
@@ -1,14 +1,36 @@
 import '../../utils/logger.dart';
 import '../../utils/store.dart';
 import '../../utils/string_values.dart';
+import '../base/choice.dart';
 import '../base/menu.dart';
 
-class ExitMenu extends Menu {
-  ExitMenu() : super(StringValues.exitMenuText);
+class ExitMenu extends Choice {
+  ExitMenu() : super(StringValues.exitMenuText){
+    addMenu(YesMenu());
+    addMenu(NoMenu());
+  }
 
   @override
-  void execute() {
+  void execute() {  
+    Log.info("Do you want to save the file");
+    super.execute();
     Log.info("Exiting application.... ");
     Store.instance.isRunning = false;
   }
+}
+
+
+class YesMenu extends Menu {
+  YesMenu() : super("Yes");
+
+  @override
+  void execute() {
+    Store.instance.saveStore();
+  }
+}
+class NoMenu extends Menu {
+  NoMenu() : super("No");
+
+  @override
+  void execute(){}
 }
